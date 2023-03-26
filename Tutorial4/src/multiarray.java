a094526204d79c73a0dd34b7f5b23e360a25262d
Sudesh Tharoosh <124483279+SudeshTharoosh@users.noreply.github.com>
2023-03-26 07:34:50 +0530

    Delete Tutorial4 directory

--- Tutorial4/src/multiarray.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class multiarray {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int[][] marks = new int[5][3];
-
-
-    }
-}
